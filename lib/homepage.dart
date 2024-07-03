48c0b0ce74c395a0d51a0752ac6b4ca43b13a1cb
syed <syedzubyl96@gmail.com>
2024-07-03 22:52:07 +0530

    syed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,12 +9,24 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text("DATA GET FROM YOUR"),),
+        title: const Center(child: Text("DATA GET FROM YOUR",
+        ),),
         actions: [
           IconButton( icon: Icon(
            isDarkMode? Icons.wb_sunny : Icons.nights_stay),
            onPressed: onThemeChange, 
           ),
+        ],
+      ),
+      body: const Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Text("tittle"
+            ,style: TextStyle(
+                fontSize: 24
+              ),),
+          )
         ],
       ),
     );
